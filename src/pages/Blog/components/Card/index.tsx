cfab9598ce9e600d40856eabbd4d4346ece40cc0
Bruno Becoski <brunobecoski@gmail.com>
2022-08-30 23:08:07 -0300

    Carregando informações do perfil do Github

--- src/pages/Blog/components/Card/index.tsx
+++ src/pages/Blog/components/Card/index.tsx
@@ -9,7 +9,6 @@ export function Card() {
       </div>
 
       <p>Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.</p>
-
     </CardContainer>
   );
 }
--- src/pages/Blog/components/Profile/index.tsx
+++ src/pages/Blog/components/Profile/index.tsx
@@ -1,39 +1,73 @@
-import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare, faBuilding, faUserGroup } from '@fortawesome/free-solid-svg-icons';
-
-import { Avatar, ProfileContainer, Title, Info } from './styles';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
+import { api } from '../../../../lib/axios';
+
+import { Avatar, ProfileContainer, Title, Info } from './styles';
+
+interface User {
+  avatar_url: string;
+  name: string;
+  html_url: string;
+  bio: string;
+  login: string;
+  company: string;
+  followers: number;
+}
+
 export function Profile() {
+  const [user, setUser] = useState({} as User);
+  const username = 'brunobecoski';
+  
+  useEffect(() => {
+    async function fetchUser() {
+      const { data } = await api.get(`users/${username}`);
+      const { login, avatar_url, html_url, name, company, bio, followers } = data;
+
+      setUser({
+        avatar_url,
+        name,
+        html_url,
+        bio,
+        login,
+        company,
+        followers
+      })
+    }
+
+    fetchUser();
+  }, []);
+
   return (
     <ProfileContainer>
-      <Avatar src="https://www.github.com/brunobecoski.png"/>
+      <Avatar src={user.avatar_url}/>
       <div>
         <Title>
-          <h1>Bruno Becoski</h1>
-          <NavLink to="#">
+          <h1>{user.name}</h1>
+          <a href={user.html_url} target="_blank">
             GITHUB
             <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-          </NavLink>
+          </a>
         </Title>
 
-        <p>Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.</p>
+        <p>{user.bio}</p>
 
         <Info>
           <div>
             <FontAwesomeIcon icon={faGithub} />
-            brunobecoski
+            {user.login}
           </div>
 
           <div>
             <FontAwesomeIcon icon={faBuilding} />
-            Rocketseat
+            {user.company}
           </div>
 
           <div>
             <FontAwesomeIcon icon={faUserGroup} />
-            32 seguidores
+            {user.followers} seguidores
           </div>
         </Info>
       </div>
