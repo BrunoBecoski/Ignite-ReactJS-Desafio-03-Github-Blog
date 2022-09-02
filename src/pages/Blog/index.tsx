d8fa20af64205c603a3848267d1336c16952842e
Bruno Becoski <brunobecoski@gmail.com>
2022-09-01 23:17:57 -0300

    Pesquisando issues pelo título

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -20,12 +20,14 @@ interface IssuesResponse {
 
 export function Blog() {
   const [issues, setIssues] = useState<Issue []>([]);
+  const [issuesSearch, setIssuesSearch] = useState<Issue []>([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     async function fetchIssues() {
       const { data } = await api.get<IssuesResponse>('/search/issues?q=repo:rocketseat-education/reactjs-github-blog-challenge');
-      // const { data } = await api.get<IssuesResponse>('/search/issues?q=repo:rocketseat-education/bootcamp-gostack-desafios');
-      
+      // const { data } = await api.get<IssuesResponse>('/search/issues?q=repo:rocketseat-education/bootcamp-gostack-desafios'); 
+
       const issuesItems = data.items.map((issue) => (
         {
           number: issue.number,
@@ -41,6 +43,20 @@ export function Blog() {
     fetchIssues();
   }, []);
 
+  useEffect(() => {
+    const regex = new RegExp("" + search.toLocaleLowerCase() + "");
+
+    const searchIssues = issues.filter(issue => {
+      if(regex.test(issue.title.toLowerCase())) {
+        return issue;
+      } else {
+        return;
+      }
+    });
+
+    setIssuesSearch(searchIssues as Issue[]);
+  }, [search]);
+
   return(
     <BlogContainer>
       <Profile />
@@ -48,17 +64,28 @@ export function Blog() {
         <h3>Publicações</h3>
         <span>6 publicações</span>
       </Title>
-      <Input placeholder="Buscar contéudo" />
+      <Input
+        placeholder="Buscar contéudo"
+        onChange={props => setSearch(props.target.value)}  
+      />
 
       <Cards>
-        {
-          issues.map(issue => (
-            <Card
+        { issuesSearch.length === 0
+        ?
+        issues.map(issue => (
+          <Card
+            key={issue.number}
+            issue={issue}
+          />
+        ))
+        :
+        issuesSearch.map(issue => (
+          <Card
               key={issue.number}
               issue={issue}
             />
           ))
-        }
+      }
       </Cards>
     </BlogContainer>
   );
